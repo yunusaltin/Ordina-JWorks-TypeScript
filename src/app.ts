45b9857359dda6644e979536463f59a00e4823d1
Kevin Van Houtte <kevin.van.houtte1@gmail.com>
2017-08-16 22:09:33 +0200

    replace var with let

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ let jsonParser = new JSONParser();
 jsonParser.getJSON("items.json", data => {
     let library:Library = Library.fromJSON(data);
 
-    var itemContainer: HTMLElement = document.getElementById("items");
+    let itemContainer: HTMLElement = document.getElementById("items");
     library.getAll().forEach(item => item.render(itemContainer));
 
 });
